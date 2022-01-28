24a5a986026e02ac4a6d18687fd7836f521856c8
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-01-28 16:06:12 +0100

    Adds Account

---
+++ src/Account.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it } from '@jest/globals'
+import { Account } from './Account'
+
+describe('Account', () => {
+  it('can be booked on', () => {
+    const account = new Account()
+  })
+})
