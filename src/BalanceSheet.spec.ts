ff80edf45e6e35d55f62408fd3311830744efce7
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-01-28 16:09:06 +0100

    Adds BalanceSheet

---
+++ src/BalanceSheet.spec.ts
@@ -0,0 +1,8 @@
+import { describe, test } from '@jest/globals'
+import { BalanceSheet } from './BalanceSheet'
+
+describe('BalanceSheet', () => {
+  test('', () => {
+    const balanceSheet = new BalanceSheet()
+  })
+})
