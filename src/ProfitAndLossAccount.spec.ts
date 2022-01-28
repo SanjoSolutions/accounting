c11eaa68af156a5a6ae76e6f488c20ecf67dea64
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-01-28 16:11:06 +0100

    Adds ProfitAndLossAccount

---
+++ src/ProfitAndLossAccount.spec.ts
@@ -0,0 +1,8 @@
+import { describe, test } from '@jest/globals'
+import { ProfitAndLossAccount } from './ProfitAndLossAccount'
+
+describe('ProfitAndLossAccount', () => {
+  test('', () => {
+    const profitAndLossAccount = new ProfitAndLossAccount()
+  })
+})
