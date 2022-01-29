b818823c5c1bc306382c49d1eda971ad8395179c
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-01-29 18:17:04 +0100

    Function for parsing document with Google Cloud Vision API

---
+++ core/src/documentParsing/parseDocument.ts
@@ -0,0 +1,34 @@
+import { v1 as vision } from '@google-cloud/vision'
+import { google } from '@google-cloud/vision/build/protos/protos.js'
+import IAsyncBatchAnnotateFilesRequest = google.cloud.vision.v1.IAsyncBatchAnnotateFilesRequest
+
+export async function parseDocument(gcsSourceUri: string, gcsDestinationUri: string) {
+  const client = new vision.ImageAnnotatorClient()
+
+  const request = {
+    requests: [
+      {
+        inputConfig: {
+          mimeType: 'application/pdf',
+          gcsSource: {
+            uri: gcsSourceUri,
+          },
+        },
+        features: [
+          {
+            type: 'DOCUMENT_TEXT_DETECTION',
+          },
+        ],
+        outputConfig: {
+          gcsDestination: {
+            uri: gcsDestinationUri,
+          },
+        },
+      },
+    ],
+  } as IAsyncBatchAnnotateFilesRequest
+
+  const [operation] = await client.asyncBatchAnnotateFiles(request)
+  const [filesResponse] = await operation.promise()
+  const destinationUri = filesResponse.responses![0].outputConfig!.gcsDestination!.uri
+}
