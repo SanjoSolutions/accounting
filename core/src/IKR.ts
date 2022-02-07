b55fb4f2981d829e8c7d86aaf36096fdf15c5cad
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-02-07 01:30:19 +0100

    Some mapping for accounts to the balance sheet for the IKR

---
+++ core/src/IKR.ts
@@ -0,0 +1,46 @@
+import { Account } from './Account.js'
+
+/**
+ * Industrieller Kontenrahmen
+ */
+export class IKR {
+
+}
+
+export enum BalanceSheetPosition {
+  // Active
+  FixedAssets = 1,
+  CurrentAssets = 2,
+  RechnungsabgrenzungspostenAktiv = 3,
+  DeferredTaxAssets = 4,
+  ActiveDifferenceFromTheAssetCalculation = 5,
+  // Passive
+  EquityCapital = 6,
+  Accruals = 7,
+  Liabilities = 8,
+  RechnungsabgrenzungspostenPassiv = 9,
+  DeferredTaxLiabilities = 10,
+}
+
+export function determineBalanceSheetPositionForAccount(account: Account): BalanceSheetPosition | null {
+  const number = account.number
+  if (number === null) {
+    return null
+  } else if (number >= 0 && number < 2000) {
+    return BalanceSheetPosition.FixedAssets
+  } else if (number >= 2000 && number < 2900) {
+    return BalanceSheetPosition.CurrentAssets
+  } else if (number >= 2900 && number < 3000) {
+    return BalanceSheetPosition.RechnungsabgrenzungspostenAktiv
+    // FIXME: DeferredTaxAssets?
+    // FIXME: ActiveDifferenceFromTheAssetCalculation
+  } else if (number >= 3000 && number < 3700) {
+    return BalanceSheetPosition.EquityCapital
+  } else if (number >= 3700 && number < 4000) {
+    return BalanceSheetPosition.Accruals
+    // FIXME: RechnungsabgrenzungspostenPassiv
+    // FIXME: DeferredTaxLiabilities
+  } else {
+    return null
+  }
+}
